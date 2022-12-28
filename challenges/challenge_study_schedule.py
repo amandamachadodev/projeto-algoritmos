9f489c03ff82d9e7707de3ae577f6dc17000711c
mandioquynha <machado.amandag@gmail.com>
2022-12-28 18:39:34 -0300

    feat: implementado algoritmo de busca
    requisito 1 concluido

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
-'iniciando projeto'
+    if type(target_time) != int:
+        return None
+    students = 0
+    for come_in, exit in permanence_period:
+        if type(come_in) != int or type(exit) != int:
+            return None
+        elif come_in <= target_time <= exit:
+            students += 1
+    return students
