9850ea5b84072eaa9c5ad522515b065e3df28c6c
mandioquynha <machado.amandag@gmail.com>
2022-12-28 21:02:27 -0300

    feat: :sparkles: implementada função recursiva
    requisito 3 concluido

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -7,6 +7,6 @@ def test_encrypt_message():
     assert encrypt_message(message, -1) == "CCBBAA"
     assert encrypt_message(message, 3) == "BAA_CCB"
     assert encrypt_message(message, 4) == "CC_BBAA"
-    
+
     with pytest.raises(TypeError, match="tipo inválido para key"):
         encrypt_message(message, "string")
