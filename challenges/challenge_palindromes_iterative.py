2c6fc6c85b059fd53396bfd09f02c49b6271557c
mandioquynha <machado.amandag@gmail.com>
2023-01-03 19:28:56 -0300

    feat: :sparkles: palíndromo
    requisito 6 concluido

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,7 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+    for index in range(len(word) // 2):
+        if word[index] != word[-index - 1]:
+            return False
+    return True
